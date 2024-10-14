da3ffcca938225b31048ffb278797b9baa818ad7
S0mbr4xc <s3._xc@hotmail.com>
2024-10-14 11:47:18 -0500

    semicompletado

--- src/App.java
+++ src/App.java
@@ -5,5 +5,13 @@ public class App {
         Scanner leer = new Scanner(System.in);
         int[] array =  {10,20,30,40,50,60,70,80,90,100};
         buscar.printArreglo(array);
+        System.out.println("Ingrese un valor para buscar en el arreglo");
+        int buscarNumero = leer.nextInt();
+        int posicion = buscar.busquedaSecuencial(array,buscarNumero);
+        if (posicion == -1){
+            System.out.println("El elemento no se encuentra en el arreglo 404");
+        } else {
+            System.out.println("El elemento se encuentra en la posicion: " + posicion);
+        }
     }
 }
--- src/Busqueda.java
+++ src/Busqueda.java
@@ -1,4 +1,13 @@
 public class Busqueda {
+    public int busquedaSecuencial(int[] array, int valorBuscado){
+        int pos = -1;
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] == valorBuscado){
+                pos = i;
+            }
+        }
+        return pos;
+    }
     public  void printArreglo (int[] array){
         for (Object elem : array) {
             System.out.println(elem + " ");
